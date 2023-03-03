26d03a4e08f14f84537050873a26e0fb7d1183f8
Ross Cohen <rosscohen1990@gmail.com>
2023-03-03 18:54:25 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv'
 import Filter from 'bad-words'
 import { rateLimitMiddleware } from './middlewares/rateLimitMiddleware.js'
 
-const allowedOrigins = ['http://eyucoder.com', 'https://chatgpt.eyucoder.com', 'http://localhost']
+const allowedOrigins = ['http://eyucoder.com', 'https://chatgpt.eyucoder.com', 'https://new-chat-ui-gpt.vercel.app']
 
 const filter = new Filter()
 
